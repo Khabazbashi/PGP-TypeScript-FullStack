d245ec87e221732426dd4230073c3bc7c658e417
Sara Khabazbashi <sara.khabazbashi@gmail.com>
2022-10-21 16:20:50 +0200

    Fetch puppies into puppies state and render through puppieslist and basepuppy component

--- frontendpuppies/src/App.tsx
+++ frontendpuppies/src/App.tsx
@@ -1,22 +1,26 @@
-import React from "react";
-import { FetchPuppies } from "./puppies/puppies.api";
-import { Puppy } from "./puppies/puppy.interface";
+import React, { useState, useEffect } from "react";
+import { FetchPuppies } from "./puppies.api";
+import { Puppy } from "./puppy.interface";
+import PuppiesList from "./components/PuppiesList";
 import "./App.css";
 
 function App() {
-  FetchPuppies<Puppy[]>("http://localhost:7000/api/puppies/").then(
-    (toDoItem) => {
-      let myNewToDo: Puppy[] = toDoItem;
-      console.log(myNewToDo);
-    }
-  );
+  const [puppies, setPuppies] = useState<Puppy[]>([]);
+
+  useEffect(() => {
+    FetchPuppies<Puppy[]>("http://localhost:7000/api/puppies/").then(
+      (puppies) => {
+        let puppiesList: Puppy[] = puppies;
+        console.log(puppiesList);
+        setPuppies(puppiesList);
+      }
+    );
+  }, []);
 
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <PuppiesList puppies={puppies} />
       </header>
     </div>
   );
