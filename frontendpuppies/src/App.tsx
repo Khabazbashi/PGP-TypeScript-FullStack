e743d8a78d67157866fd18b686010276fb65de2b
Sara Khabazbashi <sara.khabazbashi@gmail.com>
2022-10-21 18:35:09 +0200

    Add routes to home and add puppy

--- frontendpuppies/src/App.tsx
+++ frontendpuppies/src/App.tsx
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { FetchPuppies } from "./puppies.api";
 import { Puppy as IPuppy } from "./puppy.interface";
-import PuppiesList from "./components/PuppiesList";
+import { Route, Routes, NavLink, HashRouter } from "react-router-dom";
+import Home from "./Home";
+import AddPuppy from "./AddPuppy";
 import "./App.css";
+import PuppiesList from "./components/PuppiesList";
 
 function App() {
   const [puppies, setPuppies] = useState<IPuppy[]>([]);
@@ -18,10 +21,25 @@ function App() {
   }, []);
 
   return (
-    <div className="flex flex-col min-h-screen w-screen	bg-[#282c34] items-center justify-center text-center">
-      <h1 className="font-mono text-6xl my-16 px-6">Happy Dogs</h1>
-      <PuppiesList puppies={puppies} />
-    </div>
+    <HashRouter>
+      <div className="flex flex-col min-h-screen w-screen	bg-[#282c34] items-center justify-center text-center">
+        <h1 className="font-mono text-6xl my-16 px-6">Happy Dogs</h1>
+        <ul className="header">
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/AddPuppy">Add Puppy</NavLink>
+          </li>
+        </ul>
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Home puppies={puppies} />} />
+            <Route path="/AddPuppy" element={<AddPuppy />} />
+          </Routes>
+        </div>
+      </div>
+    </HashRouter>
   );
 }
 
