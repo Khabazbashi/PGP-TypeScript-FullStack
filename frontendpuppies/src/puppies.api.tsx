9986254c26d9f0f4253a3ea7ea3d7a069dbfc54b
Sara Khabazbashi <sara.khabazbashi@gmail.com>
2022-10-23 08:43:53 +0200

    Add method for posting to puppies api

--- frontendpuppies/src/puppies.api.tsx
+++ frontendpuppies/src/puppies.api.tsx
@@ -1,5 +1,18 @@
-export function FetchPuppies<T>(resourceUrl: string): Promise<T> {
-  return fetch(resourceUrl).then((response) => {
-    return response.json();
+export async function FetchPuppies<T>(resourceUrl: string): Promise<T> {
+  const response = await fetch(resourceUrl);
+  return await response.json();
+}
+
+export async function CreatePuppy<T>(
+  resourceUrl: string,
+  content: T
+): Promise<T> {
+  const response = await fetch(resourceUrl, {
+    method: "POST",
+    body: JSON.stringify(content),
+    headers: {
+      "Content-Type": "application/json;charset=UTF-8",
+    },
   });
+  return await response.json();
 }
